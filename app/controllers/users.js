1ae5be25247e7042bbba6409a7a98adb86fa3e77
yyh123 <yunhaoyu@gmail.com>
2019-07-08 02:29:38 +0800

    用户增删改查

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,17 +1,51 @@
 
-class User {
+const User = require('../models/users')
+
+class UserCtrl {
+
+  async find(ctx) {
+    ctx.body = await User.find()
+  }
+
+  async findById(ctx) {
+    const user = await User.findById(ctx.params.id)
+    if(!user) {
+      ctx.throw(404,'该用户不存在!')
+    }
+    ctx.body = user
+  }
+
   topic(ctx) {
     ctx.body = '<h1>用户主题页!</h1>'
   }
 
-  create(ctx) {
-      ctx.verifyParams({
-        name: {type:'string', required:true},
-        age: {type: 'number', required: false}
-      })
-      ctx.body = '<h1> 创建新用户 </h1>'
+  async create(ctx) {
+    ctx.verifyParams({
+      name: {type:'string', required:true}
+
+    })
+    const user =  await new User(ctx.request.body).save();
+    ctx.body = user
   }
+
+  async update(ctx) {
+    ctx.verifyParams({
+      name: {type: 'string', required: true}
+    })
+    
+    const user = await User.findByIdAndUpdate(ctx.params.id,ctx.request.body)
+    if(!user) {ctx.throw(404,'用户不存在')}
+    ctx.body = user
+
+  }
+
+  async deleteUser(ctx) {
+    const user = await User.findByIdAndRemove(ctx.params.id)
+    if (!user) { ctx.throw(404,'用户不存')}
+    ctx.status = 204
+  }
+
 }
 
-module.exports = new User()
+module.exports = new UserCtrl()
 
--- app/routes/user.js
+++ app/routes/user.js
@@ -1,11 +1,17 @@
 const Router = require('koa-router')
-const {topic, create} = require('../controllers/users')
+const { topic, create, find, findById, update, deleteUser} = require('../controllers/users')
 
 const router = new Router({prefix: '/user'})
 
 router.get('/topic', topic)
 
+router.get('/findAll', find)
+
+router.get('/findUser/:id', findById)
+
 router.post('/create', create)
+router.put('/update/:id', update)
+router.delete('/delete/:id', deleteUser)
 
 
 module.exports = router
