7d7fd025740e43dc93e546f0bb80e8d8bce62ec7
yyh123 <yunhaoyu@gmail.com>
2019-07-08 01:36:21 +0800

    用mongoose连接mongDB.Atlas

---
+++ app/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  connectionStr: 'mongodb+srv://yyh123:yuyunhao123@cluster0-grjnu.mongodb.net/test?retryWrites=true&w=majority'
+}
--- app/index.js
+++ app/index.js
@@ -3,10 +3,21 @@ const bodyParser = require('koa-bodyparser')
 const router = require('./routes/index')
 const error = require('koa-json-error')
 const parameter = require('koa-parameter')
+const mongoose = require('mongoose')
+
+const {connectionStr} = require('./config')
 
 const app = new Koa()
 
-
+mongoose.connect(
+  connectionStr, 
+  {
+  useNewUrlParser: true
+ }, 
+ ()=> {
+  console.log('mongodb connection success!')
+})
+mongoose.connection.on('error', console.error)
 
 app.use(error({
   postFormat: (e, {stack,...rest}) => process.env.NODE_ENV === 'production' ? rest : {stack,...rest}
